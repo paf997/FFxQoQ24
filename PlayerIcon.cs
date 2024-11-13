5808caac2c55eb6e47b389fadda9aeec641fa55c
Paf997 <paf997@usask.ca>
2024-11-13 10:28:01 -0600

    Lots of changes to Player Frame/Icon

---
+++ PlayerIcon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerIcon : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI hpIcon;
+    [SerializeField] PlayerStats ps;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setUpStats(){
+        hpIcon.text = ps.startingHP.ToString();
+    }
+
+    public void attAction(){
+        int dmgDealt = ps.
+    }
+}
